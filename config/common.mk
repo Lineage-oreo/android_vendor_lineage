964437393f445438f252d8f78b29d455164df47f
beanstown106 <nbenis106@gmail.com>
2017-03-18 15:35:25 -0700

    Set Gboard theme to Pixel blue by default
    Change-Id: I7a7beff67ef9b17310b7e1ffee6187830dc1cae4

--- config/common.mk
+++ config/common.mk
@@ -11,7 +11,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    keyguard.no_require_sim=true
+    keyguard.no_require_sim=true \
+    ro.com.google.ime.theme_id=5
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
