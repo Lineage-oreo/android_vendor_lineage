e1b1aef7b7ecaa8fa60c4cdc014694c82fb97ca4
Adrian DC <radian.dc@gmail.com>
2017-12-21 12:23:34 +0000

    soong_config: Add multiple product variables
    *has_legacy_camera_hal1
    *uses_media_extensions
    
    Change-Id: Ie50711a593523755fe3037688b121192ba34e841

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -9,6 +9,13 @@ type Product_variables struct {
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
+
+	Uses_media_extensions struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -16,4 +23,6 @@ type ProductVariables struct {
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Uses_media_extensions   *bool `json:",omitempty"`
 }
